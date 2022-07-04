1253e6d1911e4c2f1faee04aa90c888c51d717f0
mohasb <72586082+mohasb@users.noreply.github.com>
2022-07-04 11:57:38 +0200

    Workspace Eclipse

---
+++ !eclipse-workspace-MuhammadHichoHaidor/Clase1.1_Tabla3/src/muhammad/Ejercici1_1.java
@@ -0,0 +1,23 @@
+package muhammad;
+
+
+/**
+Realiza un aplicación Java que muestre por pantalla la tabla del 3, usando las estructura while
+ */
+public class Ejercici1_1
+{
+   public static void main(String [] args){
+       final int MAX=10;
+       int i=0;
+       while(i<=MAX){
+           System.out.println(i+" x 3 = "+i*3);
+           i++;
+           //System.out.println(3*i++);
+
+        }
+    
+    
+    }
+    
+    
+}
---
+++ !eclipse-workspace-MuhammadHichoHaidor/Clase1.1_Tabla3/src/muhammad/Tabla3.java
@@ -0,0 +1,21 @@
+package muhammad;
+
+
+public class Tabla3
+{
+    public static void main(String[] args) {
+        
+        int tabla = 3;
+        int multiplicador = 0;
+        
+        System.out.println("Tabla del 3: ");
+
+        
+        while (multiplicador <= 10) {
+            
+            System.out.println(tabla + " * " + multiplicador + " = " + (tabla * multiplicador));
+            
+            multiplicador++;
+        }
+    }
+}
